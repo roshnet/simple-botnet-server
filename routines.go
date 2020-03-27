683866e54f3b6d46f68f58836f52faeafdd44c9c
Roshan Sharma <reprogram46@gmail.com>
2020-03-27 19:41:49 +0530

    feat: Create server with multiple goroutines

--- routines.go
+++ routines.go
@@ -9,3 +9,9 @@ func strike(url string, c chan string) {
 	}
 	c <- resp.Status + " : " + url
 }
+
+func multiStrike(url string, N uint64, c chan string) {
+	for i := N - 1; i >= 0; i-- {
+		strike(url, c)
+	}
+}
--- server.go
+++ server.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
 )
 
+func createAndRunBotnet(url string, n uint64, c chan string) {
+	port := "4000"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		go multiStrike(url, n, c)
+		fmt.Fprintf(w, "[RECV]")
+	})
+	http.ListenAndServe(":"+port, nil)
+}
+
 func main() {
 	if len(os.Args) < 3 {
 		panic("Forgot something maybe?")
 	}
-	TLCallbacks, typeError := strconv.Atoi(os.Args[2])
+	TLCallbacks, typeError := strconv.ParseUint(os.Args[2], 0, 64)
 	if typeError != nil {
 		panic("Cannot understand supplied value of top level callbacks.")
 	}
-	url := os.Args[1] // Add test cases to validate URL structure
+
+	url := os.Args[1] // TBD: Add test cases to validate URL structure
 	c := make(chan string)
 
+	go createAndRunBotnet(url, TLCallbacks, c)
 	fmt.Println("[STANDBY] T minus 3 seconds...")
 	time.Sleep(time.Second * 3)
 
-	// Single way pinging for now...
-	for i := 0; i < TLCallbacks; i++ {
-		go strike(url, c)
-	}
-
-	for i := 0; i < TLCallbacks; i++ {
+	for i := uint64(0); i < TLCallbacks; i++ {
+		fmt.Println("Listening for callbacks...")
 		<-c
 	}
+	<-c
 }
